9c61f8932c279c40171f0073f98c7460dd78ebcf
Bilal Gujjar <bilaltahir515@gmail.com>
2020-08-06 10:40:47 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <h3>Just CLick On Allow Notification</h3>
       <button onClick={initNotification}>Configure Notification</button>
     </div>
   );
