2cf0d8651248ab2e6137d7f125adf20294531c1d
Bilal Gujjar <bilaltahir515@gmail.com>
2020-08-06 10:51:56 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <h1>Hello World</h1>
       <h3>Just CLick On Allow Notification</h3>
+      <h4>This is Test VESION OF PWA</h4>
       <button onClick={initNotification}>Configure Notification</button>
     </div>
   );
